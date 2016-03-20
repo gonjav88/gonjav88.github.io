7e1931bf173adfad0fb7a39e3495764fc7b50c4c
Gonzalo Javier <gonjavrod@gmail.com>
2016-03-20 17:19:23 -0300

    no recuerdo

--- accion.php
+++ accion.php
@@ -1,2 +1,14 @@
-Hola <?php echo htmlspecialchars($_POST['nombre']); ?>.
-Usted tiene <?php echo (int)$_POST['edad']; ?> años.
+<!DOCTYPE html>
+<html>
+<body>
+  <head>
+    <title>Hola <?php echo $_GET['nombre']; ?></title>
+    <link rel="stylesheet" type="text/css" href="main.css"/>
+    <meta charset="utf-8"/>
+  </head>
+  <p>
+	Hola <?php echo $_GET['nombre']; ?> <br>
+	Su mail es <?php echo (int)$_GET['email']; ?> 
+  </p>
+</body>
+</html>
