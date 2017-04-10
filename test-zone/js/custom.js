f739af1c21606cbeb42c9bcc32167e7fb1fca716
gonjav88 <gonjavrod@gmail.com>
2017-04-09 23:13:54 -0300

    Agrego test-zone

---
+++ test-zone/js/custom.js
@@ -0,0 +1,13 @@
+
+
+sayHello = function(){window.alert("Hello")}	
+
+
+$(document).ready(function(){
+                $("#cambletra").click(function(){
+                    $("#titulo").text("Te cambié la fuente, jojo");
+                    $("#titulo").css("font-family", "Gruppo");
+                    $("#diayhora").css("font-family", "Gruppo");
+                                            });   
+                                        });
+
