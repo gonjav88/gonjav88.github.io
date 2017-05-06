ad306ade8eef0f7b4c08cbccea38e629202b66a6
Gonzalo Javier <gonjavrod@gmail.com>
2017-05-06 19:52:48 -0300

    PHP Working
    PHP ANDANDO!

--- GreenTech/php/contact.php
+++ GreenTech/php/contact.php
@@ -2,45 +2,51 @@
 
 ## CONFIG ##
 
-# LIST EMAIL ADDRESS
+# LIST EMAIL ADDRESS ##
 $recipient = "info@greentech.com.ar";
 $recipient2 = $_REQUEST['email'];
 
 $remit_mail = $_REQUEST['email'];
 $remit_name = $_REQUEST['name'];
 
+## FORM VALUES ##
+$name = $_REQUEST['name'];
+$human = $_REQUEST['human'];
+$message = $_REQUEST['message'];
+$formsubject = $_REQUEST['subject'];
 
 # SUBJECT (Subscribe/Remove)
 $subject = "Mail de Consulta de $remit_name $remit_mail";
-$subject2 = "Consulta recibida";
+$subject2 = "GreenTech - Consulta recibida. Asunto: $formsubject";
 
 # RESULT PAGE
 $location = "http://www.greentech.com.ar";
 
-## FORM VALUES ##
 
-# SENDER - WE ALSO USE THE RECIPIENT AS SENDER IN THIS SAMPLE
-# DON'T INCLUDE UNFILTERED USER INPUT IN THE MAIL HEADER!
+
+# SENDER 
 $sender = $recipient;
 
 # MAIL BODY
 $body .= "Name: ".$_REQUEST['name']." \n";
 $body .= "Email: ".$_REQUEST['email']." \n";
-$body .= "Email: ".$_REQUEST['subject']." \n";
-$body .= "Email: ".$_REQUEST['message']." \n";
+$body .= "Asunto: ".$_REQUEST['subject']." \n";
+$body .= "Mensaje: ".$_REQUEST['message']." \n";
 
-# add more fields here if required
-$body2 .= "Gracias por ponerse en contacto con nosotros, en breve estaremos respondiendo su consulta. \n";
+# MAIL 2 BoDy #
+$body2 .= "Gracias $remit_name por ponerse en contacto con nosotros, en breve estaremos respondiendo su consulta. \n";
 $body2 .= "Muchas gracias \n";
+$body2 .= "\n Mensaje original: $message";
 
 
 ## SEND MESSGAE ##
-
-mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be sent.");
-mail( $remit_mail, $subject2, $body2, "From: $sender") or die ("Mail rta not be sent");
-
-## SHOW RESULT PAGE ##
-
-header( "Location: $location" );
+if ($human == 5) {
+    mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be sent.");
+    mail( $remit_mail, $subject2, $body2, "From: $sender") or die ("Mail rta not be sent");
+    ## RESULT PAGE ##
+    header( "Location: $location" );
+} else {
+    header("Location: http://greentech.com.ar/form-error.html");
+}
 
 ?>
