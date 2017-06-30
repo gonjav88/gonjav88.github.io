14fa85e57678151d5410203bf97ad2a1ec06d671
Gonzalo Javier <gonjavrod@gmail.com>
2017-06-30 14:17:13 -0300

    Pasaje GreenTech a php

---
+++ GreenTech/contacto.php
@@ -0,0 +1,147 @@
+<?php 
+$page_name = "Quienes Somos";
+include ('head.php');
+include ('navbar.html');
+?>
+		
+		<main class="site-content" role="main">
+		
+        <!--
+        Home Slider
+        ==================================== -->
+		
+		<section id="home-slider">
+            <div id="slider" class="sl-slider-wrapper">
+
+				<div class="sl-slider">
+				
+					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+
+						<div class="bg-img bg-img-7"></div>  <!-- ver en slit-slider.css -->
+
+						<div class="slide-caption">
+                            <div class="caption-content">
+                                <h2 class="animated bounceInLeft">¿QUIENES SOMOS?</h2>
+                                <span class="animated fadeInDown">Te invitamos a que conozcas nuestros valores y principios</span>
+                               
+                            </div>
+                        </div>
+						
+					</div>		
+					
+				</div><!-- /sl-slider -->
+			</div><!-- /slider-wrapper -->
+		</section>
+		
+        <!--
+        End Home SliderEnd
+        ==================================== -->
+			
+			<!-- about section -->
+			
+			<section id="about" >
+				<div class="container">
+				 <div class="row">
+				   <div class="col-sm-12 col-md-12 wow animated fadeInLeft">
+							<div class="recent-works">
+								<h1 class="white text-center"></h1>
+								
+									<div class="aboutext">
+										<h4>Somos una empresa dedicada a ofrecer soluciones <b>integrales</b> en materia de eficiencia energética, energía renovables, gestión ambiental y gestión de calidad, generando <b>ahorro</b> y mejorando la posición de su empresa ante la competencia, cumpliendo con los requisitos normativos y habilitaciones correspondientes.</h4>
+										<h4>Contamos con un equipo de trabajo multidisciplinario, conformado por personas competentes y eficaces que poseen el entusiasmo y determinación que nos ayuda a superarnos día a día.  Así mismo, brindamos servicios para pequeños comercios y viviendas, pudiendo hacer de su establecimiento u hogar un ambiente <b>confortable, sustentable y eficiente</b>.</h4>
+									</div>
+								
+						</div>
+					</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 col-sm-6 col-xs-12 wow animated fadeInLeft" style="margin:auto">
+							<div class="recent-works">
+								<h3 class="text-center">MISION</h3>
+								<div class="aboutext">
+									<ul style="list-style-type:none">
+										<p>Permitir a nuestros clientes, a partir de soluciones integrales, transformar su establecimiento
+											en uno más sustentable y eficiente, haciendo un uso inteligente y responsable de los recursos, 
+											para su propio beneficio y el de toda la sociedad.</p>
+                                        <p>De esta forma lograr generar un considerable ahorro disminuyendo su huella e impacto en el ambiente
+											que lo rodea siendo más competitivos.</p>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6 col-xs-12 wow animated fadeInRight" data-wow-delay="0.3s" style="margin:auto">
+							<div class="recent-works">
+								<h3 class="text-center">VISION</h3>
+									<div class="aboutext">
+									<ul style="list-style-type:none">
+										<p>Como apostamos a un futuro sustentable, aspiramos a ser líderes en el mercado brindando un producto integral 
+										gracias a nuestro equipo interdisciplinario. Logrando concientizar sobre la importancia de las energías renovables
+										y la optimización de los recursos, manteniendo la calidad de vida a través de la obtención y uso de los mismos bienes y servicios, 
+										empleando menos recursos energéticos.</p>  <!-- REESCRIBIR BIEN!! Y ACHICAR FUENTE EN CELULAR-->
+									</ul>	
+									</div>
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</section>
+			<!-- end about section -->
+				
+			<!-- Testimonial section -->
+			<section id="testimonials" class="parallax">
+				<div class="overlay">
+					<div class="container">
+						<div class="row">
+						
+							<div class="sec-title text-center white wow animated fadeInDown">
+								<h2>Valores Primordiales</h2>
+								<img src="img/parallax/VALORES.png" alt="Nuestos Valores">
+							</div>
+							<div id="workas">
+									<div class="val-item col-md-3 col-sm-6 col-xs-12 text-center wow animated fadeInUp">
+										<h4><b>Sustentabilidad</b></h4>
+										<p>Incentivamos el uso de energías renovables, tecnologías más limpias y el uso eficiente de los recursos energéticos.</p>
+									</div>
+									
+									<div class="val-item col-md-3 col-sm-6 col-xs-12 text-center wow animated fadeInUp">
+									    <h4><b>Innovación</b></h4>
+										<p>La búsqueda de la mejora continua nos permite ser una empresa destacada en nuestra actividad.</p>
+									</div>
+									<div class="val-item col-md-3 col-sm-6 col-xs-12 text-center wow animated fadeInUp">
+									    <h4><b>Responsabilidad social</b></h4>
+										<p>Estamos en la búsqueda constante de mejorar la calidad de vida de las generaciones presentes y futuras.</p>
+									</div>
+                                    <div class="val-item col-md-3 col-sm-6 col-xs-12 text-center wow animated fadeInUp">
+									    <h4><b>Eficacia y excelencia</b></h4>
+										<p>Brindamos un servicio integral de acuerdo a las necesidades de cada cliente, asumiendo el mayor compromiso.</p>
+									</div>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="sec-title text-center white wow animated fadeInDown">
+								<h2>Política Ambiental</h2>
+							</div>
+							
+							<img src="img/parallax/politica.png" alt="Políticas Ambientales" class="img-responsive"/>
+							<ul>
+								<li><p>Proteger el medioambiente y los recursos naturales.</p></li>
+								<li><p>Hacer que nuestros clientes cumplan con la legislación aplicable, disminuyan el consumo de energía elécrica y recursos hídricos, realicen estudios ambientales en todos los nuevos proyectos e instalaciones, apliquen avances tecnológicos e implementen tecnologías más limpias mejorando continuamente su acción ambiental.</p></li>
+								<li><p>Nos comprometemos a brindar acciones permanentes de formación, sensibilización y motivación sobre la protección ambiental para nuestro equipo de trabajo y de nuestros clientes</p></li>
+								<li><p>Nos comprometemos a ofrecer charlas y cursos a la comunidad.</p></li>
+							</ul>
+                        </div>
+					</div>
+				</div>
+			</section>
+			<!-- end Testimonial section -->
+			
+		
+		</main>
+		
+
+<?php 
+include ('footer.html');
+?>
---
+++ GreenTech/energias.php
@@ -0,0 +1,95 @@
+<?php 
+$page_name = "Quienes Somos";
+include ('head.php');
+include ('navbar.html');
+?>
+
+	<main class="site-content" role="main">
+        
+         <!--
+        Home Slider
+        ==================================== -->
+		
+		<section id="home-slider">
+            <div id="slider" class="sl-slider-wrapper">
+
+				<div class="sl-slider">
+				
+					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+
+						<div class="bg-img bg-img-9"></div>  <!-- ver en slit-slider.css -->
+
+						<div class="slide-caption">
+                            <div class="caption-content">
+                                <h2 class="animated bounceInLeft">Energías Renovables</h2>
+                                <span class="animated fadeInDown">¿Por qué pensar en ellas?</span>
+                                
+                            </div>
+                        </div>
+						
+					</div>		
+					
+				</div><!-- /sl-slider -->
+			</div><!-- /slider-wrapper -->
+		</section>
+		
+        <!--
+        End Home SliderEnd
+        ==================================== -->
+        <section id="eerr">
+            <div class="container">
+                <div class="eerr-title">
+                    <h3>¿Qué son las energías renovables?</h3>
+                </div>
+                <div class="eerr-exp">
+                <p>Las energías renovables nos permiten lograr independencia de las fuentes convencionales y de las limitaciones que acarreran, como la finitud de los recursos, la interrupción de los servicios, el aumento inevitable de las tarifas, la contaminación, entre otras. </p>
+                <p>Las tecnologías disponibles son varias, sin embargo para escalas pequeñas (hogares, soluciones aisladas, etc.) las más usuales son la energía solar directa y la eólica. La energía solar directa, a su vez, se puede dividir en fotovoltaica -para generar electricidad- y en solar térmica -para aprovechar el poder calórico de la misma-.</p>
+                </div>
+				
+            </div>
+        
+        
+        </section>
+                
+        
+        <!-- video -->
+		<section id="vid-slider">
+             <div class="container">
+                 
+                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="parallax-vid">
+                        <div class="vid-title">
+                            <h3>Energía Fotovoltaica</h3>
+                        </div>
+                        <div align="center" class="el-video">
+                            <video loop="loop" autoplay="autoplay" alt="funcionamiento de sistema fotovoltaico">
+                                <source type="video/mp4" src="video.mp4"/>
+                            </video>               
+                        </div>
+                    </div>   
+                 </div>
+                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="parallax-vid">
+                        <div class="vid-title">
+                            <h3>Energía Solar Térmica</h3>
+                        </div>
+                        <div align="center" class="el-video">
+                            <video loop="loop" autoplay="autoplay" alt="funcionamiento de sistema térmico solar">
+                                <source type="video/mp4" src="videotst.mp4"/>
+                            </video>               
+                        </div>
+                    </div>   
+                 </div>
+                 
+             </div>            
+        </section>
+        <!-- /video -->
+	   
+        
+
+	</main>
+
+<?php 
+include ('footer.html');
+?>
+	
