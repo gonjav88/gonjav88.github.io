03cf9369ac1f2b2878bab2f4d728e01b66793f68
Gonzalo Javier <gonjavrod@gmail.com>
2017-06-30 04:09:49 -0300

    ehm...

---
+++ Steffen/judiciales.php
@@ -0,0 +1,207 @@
+<?php 
+
+$page_name = "Servicios Judiciales";
+
+include('navbar.php');
+
+?>
+
+<div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Servicios <b>Judiciales</b></h2>
+                    <hr>
+                </div>
+				<h4 style="padding-bottom:.25em; padding-top:1em; text-transform:none; letter-spacing:0;">Lo asesoramos y asistimos en los siguientes ramos jurídicos:</h4>
+                
+                
+                <div class="serv-jud">
+                    
+                    <div class="col-lg-9 col-md-8 col-sm-12 col-xs-12 panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        <!-- 1 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingOne">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+									Derecho del Consumidor
+									</a>
+                                </h4>
+                            </div>
+                            <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                                <div class="panel-body">
+                                    Conjunto de normas destinadas a la protección del consumidor o usuario en el mercado de bienes y servicios, otorgándole y regulando ciertos derechos y obligaciones (v. gr. Conflictos derivados de la compra de bienes a través soporte digital, o falla de fábrica de bienes, o la no entrega de estos).- 
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 2 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingTwo">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+									Derecho Comercial
+									</a>
+                                </h4>
+                            </div>
+                            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                                <div class="panel-body">
+                                    Rama del Derecho privado, que regula el conjunto de normas relativas a los comerciantes en el ejercicio de su profesión, a los actos de comercio legalmente calificados como tales y a las relaciones jurídicas derivadas de la realización de estos.-
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 3 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingThree">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+									Derecho Empresarial – De Los Negocios
+									</a>
+                                </h4>
+                            </div>
+                            <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                                <div class="panel-body">
+                                    Rama del Derecho que se enfoca en la actividad empresarial y comercial de las empresas, siendo una de las principales ramas el derecho comercial (v. gr. Crisis de la empresa).-
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 4 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingFour">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+									Derecho de Familia
+									</a>
+                                </h4>
+                            </div>
+                            <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+                                <div class="panel-body">
+                                    Conjunto de normas e instituciones jurídicas que intentan regular las relaciones personales y patrimoniales de los integrantes de una familia, entre sí y respecto a terceros (v. gr. Divorcios, acción de impugnación de paternidad, alimentos, régimen de comunicación, cuidado personal de hijos, etc.)
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 5 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingFive">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+									Derecho Laboral
+									</a>
+                                </h4>
+                            </div>
+                            <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+                                <div class="panel-body">
+                                    Rama del Derecho cuyos principios y normas jurídicas tienen por objeto la tutela del trabajo humano realizado en forma libre, por cuenta ajena, en relación de dependencia y a cambio de una contraprestación (v. gr. Juico de despido, de diferencia salarial, de aguinaldos, etc.)
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 6 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingSix">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+									Derecho Societario
+									</a>
+                                </h4>
+                            </div>
+                            <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
+                                <div class="panel-body">
+                                    Rama del Derecho privado, mercantil, empresarial y corporativo que regula y estudia las sociedades y los o contratos asociativos, adquiriendo especial importancia el estudio y regulación de la Sociedad Anónima (v. gr. Acción de impugnación de acta asamblearia, acción de nulidad, tramites por ante la DPPJ).-
+                                </div>
+                            </div>
+                        </div>
+                       <!-- 7 --> 
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingSeven">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+									Derecho Tributario
+									</a>
+                                </h4>
+                            </div>
+                            <div id="collapseSeven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSeven">
+                                <div class="panel-body">
+                                    Normas y principios que regulan la relación jurídico - tributaria que nace entre la administración y el contribuyente, al producirse el hecho generador del tributo (v. gr. Descargos de sumarios instruidos por organismos recaudadores, apelaciones ante el TFN, etc.)
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 8 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingEight">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+									Derecho Civil
+									</a>
+                                </h4>
+                            </div>
+                            <div id="collapseEight" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingEight">
+                                <div class="panel-body">
+                                    Conjunto de normas jurídicas y principios del Derecho que regulan las relaciones personales o patrimoniales, voluntarias o forzosas, entre personas privadas o públicas, tanto físicas como jurídicas, de carácter privado y público, o incluso entre las últimas, siempre que actúen desprovistas de imperium o autotutela.-
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 9 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingNine">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+									Derecho Penal
+									</a>
+                                </h4>
+                            </div>
+                            <div id="collapseNine" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingNine">
+                                <div class="panel-body">
+                                    Rama del Derecho Público que regula la potestad punitiva del Estado, asociando a hechos, estrictamente determinados por la ley, como presupuesto, una pena, medida de seguridad o corrección como consecuencia (v. gr. Pedido de excarcelación, formulación de denuncias o querellas, etc.)
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 10 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingTen">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
+								    Derecho sucesorio
+									</a>
+                                </h4>
+                            </div>
+                            <div id="collapseTen" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTen">
+                                <div class="panel-body">
+                                    Parte del Derecho privado que regula la sucesión mortis causa y determina el destino de las titularidades y relaciones jurídicas tanto activas como pasivas de una persona después de su muerte (v. gr. Iniciación de juicios sucesorios, partición de herencia, confección de testamentos, etc.)
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 11 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingEleven">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEleven" aria-expanded="false" aria-controls="collapseEleven">
+									Derecho Administrativo
+									</a>
+                                </h4>
+                            </div>
+                            <div id="collapseEleven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingEleven">
+                                <div class="panel-body">
+                                    Conjunto de normas jurídicas que regula la organización, funcionamiento y atribuciones de la Administración pública en sus relaciones con los particulares y con otras Administraciones Públicas.
+                                </div>
+                            </div>
+                        </div>
+                    </div> <!-- /col -->
+                </div> <!-- /serv-jud -->
+                
+               <div class="clearfix"></div>
+                
+            </div> <!-- /box -->
+        </div> <!-- /row -->
+
+		
+     </div>   <!-- /.container -->
+
+
+
+
+<?php
+
+include('footer.html');
+
+?>
